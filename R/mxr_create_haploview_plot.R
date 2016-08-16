7cc3ac6857a83781677b97951255275b981b1dfb
unclesho <roslen@gmail.com>
2016-08-16 15:10:48 +0800

    Fixed the default parameters of mxr_create_haploview_plot()

--- R/mxr_create_haploview_plot.R
+++ R/mxr_create_haploview_plot.R
@@ -10,9 +10,9 @@
 #' @param path_to_haploview The fullpath to the Haploview.jar.
 #' @param ped_file The fullpath to the PED file in haploview format.
 #' @param info_file The fullpath to the corresponding INFO file of the PED file.
-#' @param max_distance The window size in kb of the LD block.
+#' @param max_distance_kb The window size in kb of the LD block. (DEFAULT: 200)
 #' @param memory_mb The memory to set aside for the haploview run.
-#'   (DEFAULT:2048MB)
+#'   (DEFAULT: 2048)
 #' @param out_prefix The output prefix for the generated files.
 #' @param verbose (Optional) Show verbose output. (DEFAULT=FALSE)
 #' @return TRUE if the PLINK run completed successfully. FALSE, otherwise.
@@ -21,7 +21,7 @@
 mxr_create_haploview_plot <- function(path_to_haploview="",
                                       ped_file = "",
                                       info_file = "",
-                                      max_distance = 200,
+                                      max_distance_kb = 200,
                                       memory_mb = 2048,
                                       out_prefix = "",
                                       verbose = FALSE) {
